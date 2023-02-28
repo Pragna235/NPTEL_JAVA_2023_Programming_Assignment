aa8ea20f8b834ca041f421f1565b78ec336fad40
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-02-28 23:08:05 +0530

    Add files via upload
    28th February 2023

---
+++ Week1P5.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Exercise1_2 {
+       public static void main(String[] args) {
+Scanner s = new Scanner(System.in);
+        int x = s.nextInt();
+        int y = s.nextInt();
+int z = s.nextInt();
+int result = 0;
+//Use if...else ladder to find the largest among 3 numbers and store the largest number in a variable called result.
+if(x>y)
+  if(x>z)
+    result=x;
+  else
+    result=z;
+else if(y>z)
+    result=y;
+  else
+    result = z;
+
+System.out.print(result);
+ }
+}
