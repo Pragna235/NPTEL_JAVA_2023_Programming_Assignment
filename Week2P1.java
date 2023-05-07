887f6eb5ada312aee89c10d94ac00b8c1a012e35
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-05-07 11:23:51 +0530

    Create Week2P1.java

---
+++ Week2P1.java
@@ -0,0 +1,35 @@
+//Complete the code segment to call the method  print() of class School first and then call print() method of class Student.
+
+//NOTE: Don't provide any INPUT in Sample Test Cases
+
+// This is the class named School
+class School { 
+    // This is a method in class School
+    public void print() { 
+		System.out.println("Hi! I class SCHOOL."); 
+    } 
+} 
+// This is the class named Student
+class Student { 
+	// This is a method in class Student
+    public void print() { 
+		System.out.println("Hi! I am class STUDENT"); 
+    } 
+}
+
+public class Question21{ 
+    public static void main(String args[]){
+
+		// Creating object of class Student
+		Student student = new Student();
+
+		// Creating object of class School
+		School school = new School();
+
+		// Call 'print()' method of class School
+		school.print();
+
+                // Call 'print()' method of class Student 
+		student.print();
+     } 
+}
