fe17ac16f0bf8adc796e048eb9d80d76c0872568
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-05-07 13:25:05 +0530

    Update Week1P4.java

--- Week1P4.java
+++ Week1P4.java
@@ -1,3 +1,9 @@
+//Complete the code segment to check whether the number is an Armstrong number or not.
+
+//Armstrong Number:
+
+//A positive number is called an Armstrong number if it is equal to the sum of cubes of its digits for example 153 = 13+53+33, 370, 371, 407, etc.
+
 import java.util.Scanner;
 public class Exercise1_4 {
     public static void main(String[] args) {
