2e1ce69c39754826a4b63dbc923f672dc76d1673
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-05-07 13:38:59 +0530

    Create Week4P5.java

---
+++ Week4P5.java
@@ -0,0 +1,43 @@
+//Modify the code segment to print the following output.
+
+//-----------------OUTPUT-------------------
+//Circle: This is Shape1
+//Circle: This is Shape2
+//-------------------------------------------------
+// Interface ShapeX is created
+interface ShapeX {
+ public String base = "This is Shape1";
+ public void display1();
+}
+
+// Interface ShapeY is created which extends ShapeX
+interface ShapeY extends ShapeX {
+ public String base = "This is Shape2";
+ public void display2();
+}
+
+// Class ShapeG is created which implements ShapeY
+class ShapeG implements ShapeY {
+ public String base = "This is Shape3";
+ // Overriding method in ShapeY interface
+ public void display2() {
+  System.out.println("Circle: " + ShapeY.base);
+ }
+
+ //Overriding method in ShapeX interface
+ public void display1() {
+  System.out.println("Circle: " + ShapeX.base);
+ }
+
+}
+
+// Main class Question 
+public class Question45{
+ public static void main(String[] args) {
+  //Object of class shapeG is created and display methods are called.
+  ShapeG circle = new ShapeG();
+  circle.display2();
+  circle.display1();
+  
+ }
+}
