d8e4c4ac720eed8c294acff9505cf3707c74865a
Pragna Yanduri <109524200+Pragna235@users.noreply.github.com>
2023-05-07 14:11:14 +0530

    Create Week12P4.java

---
+++ Week12P4.java
@@ -0,0 +1,25 @@
+//A program code is given to call the parent class static method and instance method in derive class without creating object of parent class. You should write the appropriate code so that the program print the contents of static method() and instance method () of parent class.
+
+class Parent {
+    public static void testClassMethod() {
+        System.out.println("The static method.");
+    }
+    public void testInstanceMethod() {
+        System.out.println("The instance method.");
+    }
+}
+public class Child extends Parent {
+   public static void testClassMethod() { }
+public static void main(String[] args) {
+        
+     // Call the instance method in the Parent class 
+    Child c= new Child();
+    c.testInstanceMethod();
+		
+     // Call the static method in the Parent class
+    Parent.testClassMethod();
+    
+    //Parent ob=c;
+   //ob.testClassMethod();
+  }
+}
